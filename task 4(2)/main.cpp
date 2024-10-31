1ab1eb7de4a6e0bf229ba0d35483f151be5d4648
Noorulainfaisal838 <faisalnoorulain7@gmail.com>
2024-10-31 22:09:50 +0500

    first commit

---
+++ task 4(2)/main.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int i=1;
+    while(i<=3)
+    {Q
+    cout<<"week"<<i<<endl;
+    int j=1;
+    while(j<=7)
+    {
+        cout<<"day"<<j<<endl;
+        j++;
+    }
+    i++;
+    }
+}
+
